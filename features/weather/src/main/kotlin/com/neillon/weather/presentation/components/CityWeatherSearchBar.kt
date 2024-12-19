df093622a89478808181f727c3ca08998bff6518
nmoura <neillon.moura@siriusxm.com>
2024-12-19 09:31:15 -0300

    Solving text clearing bug

--- features/weather/src/main/kotlin/com/neillon/weather/presentation/components/CityWeatherSearchBar.kt
+++ features/weather/src/main/kotlin/com/neillon/weather/presentation/components/CityWeatherSearchBar.kt
@@ -1,5 +1,6 @@
 package com.neillon.weather.presentation.components
 
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -16,6 +17,7 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -27,6 +29,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.neillon.weather.R
+import com.neillon.weather.presentation.TAG
 import com.neillon.weather.presentation.WeatherSearchState
 
 @Composable
@@ -36,8 +39,7 @@ fun CityWeatherSearchBar(
     onSearchUpdate: (prev: String, curr: String) -> Unit,
     onSearch: (String) -> Unit
 ) {
-    var text by rememberSaveable { mutableStateOf(searchState.query) }
-
+    var text by remember(searchState.query) { mutableStateOf(searchState.query) }
     Box(
         modifier = modifier
             .fillMaxWidth()
